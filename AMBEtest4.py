a363be4efd49ffcc2ad963ca6ce16eadddf37915
Steve Zingman <N4IRS@users.noreply.github.com>
2017-06-24 10:55:58 -0400

    Minor fixes and support for AMBEserver
    Added support for remote AMBEserver. DO NOT run on the same machine as AMBEserver

--- AMBEtest4.py
+++ AMBEtest4.py
@@ -27,13 +27,39 @@ import thread
 import time
 import string
 from time import sleep
+import socket
+
+_sock = None
+useSerial = True
+ip_address = "127.0.0.1"
+UDP_PORT = 2460
+
+
+reset = bytearray.fromhex("61 00 01 00 33");
+setDstar = bytearray.fromhex("61 00 0d 00 0a 01 30 07 63 40 00 00 00 00 00 00 48");
+getProdId = bytearray.fromhex("61 00 01 00 30");
+getVersion = bytearray.fromhex("61 00 01 00 31");
+setDMR = bytearray.fromhex("61 00 0D 00 0A 04 31 07 54 24 00 00 00 00 00 6F 48")
+
+silence = bytearray.fromhex("AC AA 40 20 00 44 40 80 80")
 
 def ambeSend( port, cmd ):
-	return port.write(cmd)
+    if useSerial == True:
+        return port.write(cmd)
+    else:
+        return _sock.sendto(cmd, (ip_address, UDP_PORT))
 
 def ambeRecv( port ):
-	_val = port.read(1024)
-	return len(_val), _val
+    if useSerial == True:
+        _val = port.read(1024)
+        return len(_val), _val
+    else:
+        try:
+            _sock.settimeout(2)
+            data, addr = _sock.recvfrom(1024) # buffer size is 1024 bytes
+        except:
+            data = ""
+        return len(data), data
 
 def ambeValidate( port, cmd, expect, label ):
 	print "Testing", label
@@ -50,7 +76,7 @@ def ambeValidate( port, cmd, expect, label ):
 				print ''.join('{:02x}'.format(ord(x)) for x in buffer)
 			else:
 				_packetLen = (ord(buffer[1]) * 256) + ord(buffer[2])
-				if _readLen < (_packetLen+3):
+				if _readLen != (_packetLen+4):
 					print 'Error, read', _readLen,'Bytes and AMBE header says it has',_packetLen,'bytes',label
 					print ''.join('{:02x}'.format(ord(x)) for x in buffer)
 				else:
@@ -68,67 +94,81 @@ def ambeValidate( port, cmd, expect, label ):
 
 
 def main(argv):
-	SERIAL_BAUD=230400
-	serialport = "/dev/ttyAMA0"
-	try:
-		opts, args = getopt.getopt(argv,"hns:",["serial="])
-	except getopt.GetoptError:
-		print 'AMBEtest4.py -s <serial port>'
-		print 'AMBEtest4.py -n -s <serial port> (for ThumbDV Model A)'
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt == '-h':
-			print 'AMBEtest4.py -s <serial port>'
-			sys.exit()
-		elif opt in ("-s", "--serial"):
-			serialport = arg
-		elif opt == "-n":
-			SERIAL_BAUD=460800
+    global useSerial
+    global _sock
+    global ip_address
+    
+    useSerial = True
+    SERIAL_BAUD=230400
+    serialport = "/dev/ttyAMA0"
+    try:
+        opts, args = getopt.getopt(argv,"hns:i:",["serial="])
+    except getopt.GetoptError:
+        print 'AMBEtest4.py -s <serial port>'
+        print 'AMBEtest4.py -n -s <serial port> (for ThumbDV Model A)'
+        print 'AMBEtest4.pi -i address'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'AMBEtest4.py -s <serial port>'
+            sys.exit()
+        elif opt in ("-s", "--serial"):
+            serialport = arg
+        elif opt in ("-i", "--ip"):
+            useSerial = False
+            ip_address = arg
+        elif opt == "-n":
+            SERIAL_BAUD=460800
 
-	print 'Setting serial port'
-	port = serial.Serial(serialport, baudrate=SERIAL_BAUD, timeout=1.0, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, xonxoff=False, rtscts=False, dsrdtr=False)
+    if useSerial == True:
+        try:
+            print 'Setting serial port'
+            port = serial.Serial(serialport, baudrate=SERIAL_BAUD, timeout=1.0, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, xonxoff=False, rtscts=False, dsrdtr=False)
+        except:
+            print 'Error opening serial port', serialport
+            exit(2)
+            
+        port.flushInput()
+        port.flushOutput()
 
-	port.flushInput()
-	port.flushOutput()
+        print 'Serial port parameters:'
+        print 'Port name:\t', serialport
+        print 'Baudrate:\t', str(port.baudrate)
+        print 'Byte size:\t', port.bytesize
+        print 'Parity:\t\t', port.parity
+        print 'Stop bits:\t', port.stopbits
+        print 'Xon Xoff:\t', port.xonxoff
+        print 'RTS/CTS:\t', port.rtscts
+        print 'DST/DTR:\t', port.dsrdtr
 
-	print 'Serial port parameters:'
-	print 'Port name:\t', serialport
-	print 'Baudrate:\t', str(port.baudrate)
-	print 'Byte size:\t', port.bytesize
-	print 'Parity:\t\t', port.parity
-	print 'Stop bits:\t', port.stopbits
-	print 'Xon Xoff:\t', port.xonxoff
-	print 'RTS/CTS:\t', port.rtscts
-	print 'DST/DTR:\t', port.dsrdtr
+        time.sleep(0.02)
+        rcv = port.read(10)
+        sys.stdout.write(rcv)
+    else:
+        _sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        _sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        _sock.bind(('', UDP_PORT))
+        port = _sock
 
-	time.sleep(0.02)
-	rcv = port.read(10)
-	sys.stdout.write(rcv)
-	print '*********************'
+    print '*********************'
 
-	reset = bytearray.fromhex("61 00 01 00 33");
-	setDstar = bytearray.fromhex("61 00 0c 00 0a 01 30 07 63 40 00 00 00 00 00 00 48");
-	getProdId = bytearray.fromhex("61 00 01 00 30");
-	getVersion = bytearray.fromhex("61 00 01 00 31");
 
-	ambeValidate(port, reset, bytearray.fromhex("39"), 'Reset DV3000')
-	ambeValidate(port, getProdId, bytearray.fromhex("414d4245333030305200"), 'Get Product ID')
-	ambeValidate(port, getVersion, bytearray.fromhex("563132302e453130302e585858582e433130362e473531342e523030392e42303031303431312e433030323032303800"), 'Get Version')
-	ambeValidate(port, setDstar, bytearray.fromhex("00"), 'Set DSTAR Mode')
+    ambeValidate(port, reset, bytearray.fromhex("39"), 'Reset DV3000')
+    ambeValidate(port, getProdId, bytearray.fromhex("414d4245333030305200"), 'Get Product ID')
+    ambeValidate(port, getVersion, bytearray.fromhex("563132302e453130302e585858582e433130362e473531342e523030392e42303031303431312e433030323032303800"), 'Get Version')
+    ambeValidate(port, setDstar, bytearray.fromhex("00"), 'Set DSTAR Mode')
 
-	setDMR = bytearray.fromhex("61 00 0D 00 0A 04 31 07 54 24 00 00 00 00 00 6F 48")
-	ambeValidate(port, reset, bytearray.fromhex("39"), 'Reset DV3000')
-	ambeValidate(port, setDMR, bytearray.fromhex("00"), 'Set DMR Mode')
+    ambeValidate(port, reset, bytearray.fromhex("39"), 'Reset DV3000')
+    ambeValidate(port, setDMR, bytearray.fromhex("00"), 'Set DMR Mode')
 
-	for _ in range(0,10):
-		silence = bytearray.fromhex("AC AA 40 20 00 44 40 80 80")
-		DMRAmbe = bytearray.fromhex("61 00 0B 01 01 48") + silence
-		_payload = ambeValidate(port, DMRAmbe, bytearray.fromhex("a0"), 'Decode AMBE')
-		if _payload != None:
-			DMRPCM = bytearray.fromhex("61 01 42 02 00 A0") + _payload
-			expect = bytearray.fromhex("48954be6500310b00777")
-			ambeValidate(port, DMRPCM, '', 'Encode PCM')
-		sleep(1)
+    for _ in range(0,10):
+        DMRAmbe = bytearray.fromhex("61 00 0B 01 01 48") + silence
+        _payload = ambeValidate(port, DMRAmbe, bytearray.fromhex("a0"), 'Decode AMBE')
+        if _payload != None:
+            DMRPCM = bytearray.fromhex("61 01 43 02 00 A0") + _payload
+            expect = bytearray.fromhex("48954be6500310b00777")
+            ambeValidate(port, DMRPCM, '', 'Encode PCM')
+        sleep(1)
 
 
 if __name__ == "__main__":
